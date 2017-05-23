978e9c1d46284d3e67e542440574ee8fe922587f
张松磊 <2594454098@qq.com>
2017-05-23 20:09:17 +0800

    create the AdaBoost project

---
+++ src/com/sonly/adaboost/AdaBoost.java
@@ -0,0 +1,15 @@
+package com.sonly.adaboost;
+
+/**
+ * Class: AdaBoost
+ * Description: to execute AdaBoost algorithm
+ * Company: HUST
+ * @author Sonly
+ * Date: 2017Äê5ÔÂ23ÈÕ
+ */
+public class AdaBoost {
+
+	public static void main(String[] args) {
+		
+	}
+}
---
+++ src/com/sonly/adaboost/CalcUtil.java
@@ -0,0 +1,12 @@
+package com.sonly.adaboost;
+
+/**
+ * Class: CalcUtil
+ * Description:  provide necessary calculate methods  
+ * Company: HUST
+ * @author Sonly
+ * Date: 2017Äê5ÔÂ23ÈÕ
+ */
+public class CalcUtil {
+
+}
